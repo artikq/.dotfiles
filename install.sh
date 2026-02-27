da5791caf3ce4bf48e588b7ae75662b7fb1aacfa
artikq <artur.apushev@gmail.com>
2026-02-27 16:58:15 +0300

    feat: move to stow approach

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+PARENT_DIR="$(dirname "$DOTFILES_DIR")"
+PKG_NAME="$(basename "$DOTFILES_DIR")"
+
+ADOPT=""
+if [ "$1" = "--adopt" ]; then
+  ADOPT="--adopt"
+fi
+
+stow -v --no-folding --dir="$PARENT_DIR" --target="$HOME" $ADOPT "$PKG_NAME"
